ccb52da40a76d906dd5504420f82926b91ad09e9
Gurumurthy30 <gurumurthy.00300@gmail.com>
2025-07-26 20:34:02 +0530

    v0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Stackformer",
-    version="0.1.1",
+    version="0.1.2",
     description="Modular transformer blocks built in PyTorch",
     # long_description=open("README.md", "r", encoding="utf-8").read(),
     # long_description_content_type="text/markdown",
@@ -18,7 +18,7 @@ setup(
     python_requires=">=3.9",
     packages=find_packages(exclude=["tests", "examples"]),
     install_requires=[
-        "torch>=2.6",
+        "torch>=2.0,<2.6",
         "tqdm>=4.67",
     ],
     classifiers=[
