9e1a57f045f4ad1aea209fd72edad8c3b8968e07
Gurumurthy30 <gurumurthy.00300@gmail.com>
2025-07-27 12:58:23 +0530

    dropout added

--- stackformer/modules/Feed_forward.py
+++ stackformer/modules/Feed_forward.py
@@ -3,56 +3,66 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class FF_ReLU(nn.Module):
-    def __init__(self,emb_dim,hidden_dim,device='cpu',dtype=torch.float32):
+    def __init__(self,emb_dim,hidden_dim,dropout = 0.0, device='cpu',dtype=torch.float32):
         super().__init__()
         self.relu=nn.Sequential(
             nn.Linear(emb_dim,hidden_dim,device=device,dtype=dtype),
             nn.ReLU(),
+            nn.Dropout(dropout),
             nn.Linear(hidden_dim,emb_dim,device=device,dtype=dtype),
+            nn.Dropout(dropout),
         )
     def forward(self,x):
         return self.relu(x)
     
 class FF_LeakyReLU(nn.Module):
-    def __init__(self,emb_dim,hidden_dim,negative_slope=0.1,device='cpu',dtype=torch.float32):
+    def __init__(self,emb_dim,hidden_dim, dropout = 0.0,negative_slope=0.1,device='cpu',dtype=torch.float32):
         super().__init__()
         self.l_relu=nn.Sequential(
             nn.Linear(emb_dim,hidden_dim,device=device,dtype=dtype),
             nn.LeakyReLU(negative_slope),
+            nn.Dropout(dropout),
             nn.Linear(hidden_dim,emb_dim,device=device,dtype=dtype),
+            nn.Dropout(dropout),
         )
     def forward(self,x):
         return self.l_relu(x)
     
 class FF_GELU(nn.Module):
-    def __init__(self,emb_dim,hidden_dim,device='cpu',dtype=torch.float32):
+    def __init__(self,emb_dim,hidden_dim, dropout = 0.0, device='cpu',dtype=torch.float32):
         super().__init__()
         self.gelu=nn.Sequential(
             nn.Linear(emb_dim,hidden_dim,device=device,dtype=dtype),
             nn.GELU(),
+            nn.Dropout(dropout),
             nn.Linear(hidden_dim,emb_dim,device=device,dtype=dtype),
+            nn.Dropout(dropout),
         )
     def forward(self,x):
         return self.gelu(x)
     
 class FF_Sigmoid(nn.Module):
-    def __init__(self,emb_dim,hidden_dim,device='cpu',dtype=torch.float32):
+    def __init__(self,emb_dim,hidden_dim, dropout = 0.0, device='cpu',dtype=torch.float32):
         super().__init__()
         self.sigmoid=nn.Sequential(
             nn.Linear(emb_dim,hidden_dim,device=device,dtype=dtype),
             nn.Sigmoid(),
+            nn.Dropout(dropout),
             nn.Linear(hidden_dim,emb_dim,device=device,dtype=dtype),
+            nn.Dropout(dropout),
         )
     def forward(self,x):
         return self.sigmoid(x)
     
 class FF_SiLU(nn.Module):
-    def __init__(self,emb_dim,hidden_dim,device='cpu',dtype=torch.float32):
+    def __init__(self,emb_dim,hidden_dim, dropout = 0.0, device='cpu',dtype=torch.float32):
         super().__init__()
         self.silu=nn.Sequential(
             nn.Linear(emb_dim,hidden_dim,device=device,dtype=dtype),
             nn.SiLU(),
+            nn.Dropout(dropout),
             nn.Linear(hidden_dim,emb_dim,device=device,dtype=dtype),
+            nn.Dropout(dropout),
         )
     def forward(self,x):
         return self.silu(x)
