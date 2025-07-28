40181233d2c2f946e1028952e4acd453c6cba9c1
Gurumurthy30 <gurumurthy.00300@gmail.com>
2025-07-28 11:08:46 +0530

    set to device and dtype

--- stackformer/modules/Normalization.py
+++ stackformer/modules/Normalization.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 
+# Norm = (Xn - Mean) / sqrt(var) + Bias
 class LayerNorm(nn.Module):
     def __init__(self, emb_dim, eps = 1e-5):
         super().__init__()
@@ -9,22 +10,8 @@ class LayerNorm(nn.Module):
     def forward(self, x):
         mean = x.mean(dim=-1, keepdim=True)
         var = x.var(dim=-1, keepdim=True, unbiased=False)
-        norm_x = (x - mean) / torch.sqrt(var + self.eps)
-        return norm_x 
-
-class LayerNorm(nn.Module):
-    def __init__(self, emb_dim, eps=1e-5, device='cpu', dtype=torch.float32):
-        super().__init__()
-        self.eps = eps
-        self.weight = nn.Parameter(torch.ones(emb_dim, device=device, dtype=dtype))
-        self.bias = nn.Parameter(torch.zeros(emb_dim, device=device, dtype=dtype))
-
-    def forward(self, x):
-        mean = x.mean(dim=-1, keepdim=True)
-        var = x.var(dim=-1, keepdim=True, unbiased=False)
-        norm_x = (x - mean) / torch.sqrt(var + self.eps)
-        return norm_x * self.weight + self.bias
-
+        norm_x = (x - mean) / torch.sqrt(var) + self.eps
+        return norm_x.to(device=x.device,dtype=x.dtype)
 
 # RMS = sqrt(Xn ** 2)
 # Norm = Xn / RMS
@@ -38,4 +25,4 @@ class RMSNormilization(nn.Module):
     def forward(self,x):
         rms = x.pow(2).mean(-1,keepdim=True).sqrt()
         norm = self.weight * x / (rms + self.eps)
-        return norm
+        return norm.to(device=x.device,dtype=x.dtype)
