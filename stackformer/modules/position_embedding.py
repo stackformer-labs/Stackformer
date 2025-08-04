d9df3942439ba4d6e1ec9a999e76a18061a617ef
Gurumurthy30 <gurumurthy.00300@gmail.com>
2025-08-04 20:56:12 +0530

    v0.1.4

--- stackformer/modules/position_embedding.py
+++ stackformer/modules/position_embedding.py
@@ -27,15 +27,17 @@ class AbsolutePositionEmbedding(nn.Module):
         >>> x = torch.randn(4, 32, 128)  # (B=4, T=32, D=128)
         >>> pos = pos_emb(x)  # (4, 32, 128)
     """
-    def __init__(self, seq_len, embed_dim):
+    def __init__(self, seq_len, embed_dim, device='cpu', dtype=torch.float32):
         super().__init__()
         self.seq_len = seq_len
         self.embed_dim = embed_dim
-        self.embedding = nn.Embedding(seq_len, embed_dim)
+        self.device = device
+        self.dtype = dtype
+        self.embedding = nn.Embedding(seq_len, embed_dim, device=device, dtype=dtype)
 
     def forward(self, x):
         batch_size, seq_len = x.shape[0], x.shape[1]
-        positions = torch.arange(0, seq_len)
+        positions = torch.arange(seq_len, device=self.device, dtype=self.dtype)
         abs_pos = self.embedding(positions)  # (T, D)
         out = abs_pos.unsqueeze(0).expand(batch_size, seq_len, -1)
         return out.to(device=x.device, dtype=x.dtype)
